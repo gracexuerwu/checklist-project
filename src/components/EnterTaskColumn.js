3fa7b088a50f58de980071a6dd1879564f2b9a3a
gracexuerwu <grace.xuer.wu@gmail.com>
2022-01-10 11:01:56 +0100

    Implemented form

--- src/components/EnterTaskColumn.js
+++ src/components/EnterTaskColumn.js
@@ -3,15 +3,35 @@ import "../styles/EnterTaskColumn.css";
 
 export default function EnterTaskColumn() {
     return (
-        <div className="EnterTaskColumn">
-            <div className="row">
-                <div className="col-11">
-                    <p>Enter tasks here</p>
+        // <div className="EnterTaskColumn">
+        //     <div className="row">
+        //         <div className="col-11">
+        //             <p>Enter tasks here</p>
+        //         </div>
+        //         <div className="col-1">
+        //             <i className="fas fa-pencil-alt fa-lg pencilIcon"></i>
+        //         </div>
+        //     </div>
+        // </div>
+        <form id="search-form" onSubmit="">
+            <div className="form-row">
+                <div className="col-11 btn">
+                    <input
+                        type="text"
+                        onChange=""
+                        className="form-control"
+                        id="search-input"
+                        placeholder="Pen your tasks"
+                        autoFocus="on"
+                        autoComplete="off"
+                    />
                 </div>
-                <div className="col-1">
-                    <i className="fas fa-pencil-alt fa-lg pencilIcon"></i>
+                <div className="col-1 btn search-btn">
+                    <button type="submit" className="btn btn-link" id="button-search">
+                        <i className="fas fa-pencil-alt fa-lg pencilIcon"></i>
+                    </button>
                 </div>
             </div>
-        </div>
+        </form>
     )
 }
