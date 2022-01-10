7650d7442319204486a33c3f6670b22a087b0189
Guobi Wu <guobi@Guobis-MacBook-Pro.local>
2022-01-10 21:24:12 +0800

    add callback, also dynamically render tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from "react";
 import './App.css';
 
 //import components
@@ -11,6 +11,21 @@ import Signature from './components/Signature';
 import selfCareicon from "./images/selfcare.png";
 
 function App() {
+
+  function callbackByEnterTaskColumn(task) {
+    // alert(task);
+    if (todoArray.length < 5) {
+      setTodoArray([...todoArray, { task, status: false }]);
+    }
+    else { }
+  }
+
+  const [todoArray, setTodoArray] = useState([
+    { task: 'AAA', status: false },
+    { task: 'BBB', status: false },
+    { task: 'CCC', status: false }
+  ]);
+
   //Date
   let date = new Date();
   let day = date.getDate();
@@ -27,6 +42,7 @@ function App() {
 
   let daysOfTheWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  let dayIndex = date.getDay();
 
   return (
     <div className="App">
@@ -37,16 +53,23 @@ function App() {
         </div>
         <h2>Date: {currentDate}</h2>
         <div className="daysOfTheWeek">{daysOfTheWeek.map((day, index) => {
-          return (<span key={index}>{day}</span>);
+          if (index === dayIndex) {
+            return (<strong key={index}>{day}</strong>);
+          }
+          else {
+            return (<span key={index}>{day}</span>);
+          }
         })}</div>
-        <EnterTaskColumn />
+        <EnterTaskColumn callback={callbackByEnterTaskColumn} />
         <div className="EnterMessage">**You’ve exceeded the max. number of tasks. Avoid trying to overload yourself! :)</div>
         <div className="Heading">Tasks to do</div>
-        <TaskColumn />
-        <TaskColumn />
-        <TaskColumn />
-        <TaskColumn />
-        <TaskColumn />
+        {/* <TaskColumn task="AAA" />
+        <TaskColumn task="CCC" /> */}
+        {
+          todoArray.map(({ task, status }, index) => (
+            <TaskColumn key={index} task={task} />
+          ))
+        }
         <div className="Heading">Task Tracker</div>
         <TaskTracker />
         <Signature />
--- src/components/EnterTaskColumn.js
+++ src/components/EnterTaskColumn.js
@@ -1,37 +1,60 @@
-import React from "react";
+import { useState } from "react";
 import "../styles/EnterTaskColumn.css";
 
-export default function EnterTaskColumn() {
-    return (
-        // <div className="EnterTaskColumn">
-        //     <div className="row">
-        //         <div className="col-11">
-        //             <p>Enter tasks here</p>
-        //         </div>
-        //         <div className="col-1">
-        //             <i className="fas fa-pencil-alt fa-lg pencilIcon"></i>
-        //         </div>
-        //     </div>
-        // </div>
-        <form id="search-form" onSubmit="">
-            <div className="form-row">
-                <div className="col-11 btn">
-                    <input
-                        type="text"
-                        onChange=""
-                        className="form-control"
-                        id="search-input"
-                        placeholder="Pen your tasks here . . ."
-                        autoFocus="on"
-                        autoComplete="off"
-                    />
-                </div>
-                <div className="col-1 btn search-btn">
-                    <button type="submit" className="btn btn-link" id="button-search">
-                        <i className="fas fa-pencil-alt fa-lg pencilIcon"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    )
+export default function EnterTaskColumn(props) {
+
+  const [task, setTask] = useState('Pen your tasks here . . .');
+
+  // const onChange = (event) => {
+  //   setValue(event.target.value);
+  // };
+
+  function onChange(event) {
+    setTask(event.target.value);
+  };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   props.callback(value);
+  // }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // alert(task);
+    props.callback(task);
+  }
+
+
+  return (
+    // <div className="EnterTaskColumn">
+    //     <div className="row">
+    //         <div className="col-11">
+    //             <p>Enter tasks here</p>
+    //         </div>
+    //         <div className="col-1">
+    //             <i className="fas fa-pencil-alt fa-lg pencilIcon"></i>
+    //         </div>
+    //     </div>
+    // </div>
+    <form id="search-form" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <div className="col-11 btn">
+          <input
+            type="text"
+            onChange={onChange}
+            className="form-control"
+            id="search-input"
+            placeholder={task}
+            autoFocus="on"
+            autoComplete="off"
+          />
+        </div>
+        <div className="col-1 btn search-btn">
+          <button type="submit" className="btn btn-link" id="button-search">
+            <i className="fas fa-pencil-alt fa-lg pencilIcon"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+  )
 }
