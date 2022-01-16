0cc8bb5223b86069bfba7d79bddf18909de1f158
gracexuerwu <grace.xuer.wu@gmail.com>
2022-01-16 13:13:33 +0100

    design updates

--- src/components/TaskColumn.js
+++ src/components/TaskColumn.js
@@ -3,20 +3,21 @@ import "../styles/TaskColumn.css";
 
 export default function TaskColumn(props) {
     return (
-        <div className="container">
+        <div className="TaskColumn">
             <div className="row">
-                <div className="col-11">
-                    <div className="TaskColumn-left">
-                        <p>{props.task}</p>
-                    </div>
-                </div>
                 <div className="col-1">
-                    <div className="TaskColumn-right">
+                    <div className="TaskColumn-left">
                         <i className="far fa-square fa-lg tickBox"></i>
                     </div>
                 </div>
+                <div className="col-11">
+                    <div className="TaskColumn-right float-left">
+                        <p>{props.task}</p>
+                    </div>
+                    <i class="fas fa-trash-alt float-right"></i>
+                </div>
             </div>
-        </div>
+        </div >
 
     )
 }
